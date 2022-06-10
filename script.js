9e723188c879c7a60b88b9b8363cba787e66600c
IgorSousaa <igoralvesnasc2@gmail.com>
2022-06-10 19:18:57 -0300

    Estrutura de Dados

---
+++ script.js
@@ -0,0 +1,20 @@
+let nomeSobrenome = "Igor Sousa"
+console.log(nomeSobrenome);
+
+const idade = 18
+console.log(idade);
+
+const ano = 2022
+console.log(ano - idade);
+
+const total = "Total"
+console.log (idade + ano);
+
+let texto = "Estou mexendo no console.log"
+console.log(texto);
+
+const quartaFeira = true
+console.log(quartaFeira);
+
+const semana = "true"
+console.log(typeof quartaFeira);
